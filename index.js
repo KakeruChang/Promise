f1b62edbe0537298ef8e505816721fba3470a08f
Jim <b97201019@gmail.com>
2020-06-10 23:45:47 +0800

    complete then function

--- index.js
+++ index.js
@@ -73,167 +73,153 @@ function myPromise(exector) {
 // }
 
 myPromise.prototype.then = function (onFulfilled, onRejected) {
+  // check the type of onFulfilled, onRejected
+  onFulfilled =
+    typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+  onRejected =
+    typeof onRejected === "function"
+      ? onRejected
+      : (error) => {
+          throw error;
+        };
   const vm = this;
-  if (this.status === status.fulfilled) {
-    onFulfilled(vm.value);
+
+  function resolvePromise(current, returned, resolve, reject) {
+    // check if current === origin
+    if (current === returned) {
+      console.log("Chaining cycle!");
+      return reject(new TypeError("Chaining cycle!"));
+    }
+
+    let isCalled;
+    // 2.3.3. Otherwise, if x is an object or function
+    // 2.3.4. If x is not an object or function, fulfill promise with x
+    if (
+      returned !== null &&
+      (typeof returned === "object" || typeof returned === "function")
+    ) {
+      try {
+        // 2.3.3.1 Let then be x.then
+        // 2.3.3.2 If retrieving the property x.then results in a thrown exception e, reject promise with e as the reason.
+        const then = returned.then;
+        // 2.3.3.3 If then is a function, call it with x as this, first argument resolvePromise, and second argument rejectPromise
+        if (typeof then === "function") {
+          then.call(
+            returned,
+            // resolve
+            (returnedAgain) => {
+              // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
+              if (isCalled) return;
+              isCalled = true;
+              // Recursive until returned is not a promise
+              resolvePromise(returned, returnedAgain, resolve, reject);
+            },
+            // reject
+            (error) => {
+              // 2.3.3.3.3 If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
+              if (isCalled) return;
+              isCalled = true;
+              reject(error);
+            }
+          );
+        } else {
+          resolve(returned);
+        }
+      } catch (error) {
+        // 2.3.3.3.4.1 If resolvePromise or rejectPromise have been called, ignore it.
+        if (isCalled) return;
+        isCalled = true;
+        reject(error);
+      }
+    } else {
+      resolve(returned);
+    }
   }
 
-  if (this.status === status.rejected) {
-    onRejected(vm.reason);
-  }
+  const promiseNew = new myPromise((resolve, reject) => {
+    if (this.status === status.fulfilled) {
+      setTimeout(() => {
+        try {
+          const origin = onFulfilled(vm.value);
+          resolvePromise(promiseNew, origin, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      }, 0);
+    }
 
-  if (this.status === status.pending) {
-    // store callbacks
-    this.resolvedCallbacks.push(() => {
-      onFulfilled(vm.value);
-    });
+    if (this.status === status.rejected) {
+      setTimeout(() => {
+        try {
+          const origin = onRejected(vm.reason);
+          resolvePromise(promiseNew, origin, resolve, reject);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
 
-    this.rejectedCallbacks.push(() => {
-      onRejected(vm.reason);
-    });
-  }
+    if (this.status === status.pending) {
+      // store callbacks
+      this.resolvedCallbacks.push(() => {
+        setTimeout(() => {
+          try {
+            const origin = onFulfilled(vm.value);
+            resolvePromise(promiseNew, origin, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+
+      this.rejectedCallbacks.push(() => {
+        setTimeout(() => {
+          try {
+            const origin = onRejected(vm.reason);
+            resolvePromise(promiseNew, origin, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      });
+    }
+  });
+
+  return promiseNew;
 };
 
-const test = new myPromise((resolve, reject) => {
+const test1 = new myPromise((resolve, reject) => {
   setTimeout(() => {
-    const guess = Math.random() * 100;
-    if (guess > 50) {
-      resolve(guess);
+    const guess1 = Math.random() - 0.5;
+    if (guess1 > 0) {
+      resolve(guess1);
     } else {
-      reject(guess);
+      reject(guess1);
     }
   });
 });
 
-test.then(
-  (result) => {
-    console.log(`success:${result}`);
-  },
-  (error) => {
-    console.log(`error:${error}`);
-  }
-);
+const test = new myPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(test1);
+  });
+});
 
-// myPromise.prototype.then = function (onFulfilled, onRejected) {
-//   const vm = this;
-
-//   ///
-//   function resolvePromise(promise2, x, resolve, reject) {
-//     // promise2和函数执行后返回的结果是同一个对象
-
-//     if (promise2 === x) {
-//       return reject(new TypeError("Chaining cycle"));
-//     }
-//     let called;
-//     // x可能是一个promise 或者是一个普通值
-//     if (x !== null && (typeof x === "object" || typeof x === "function")) {
-//       try {
-//         let then = x.then; // 取对象上的属性 怎么能报异常呢？(这个promise不一定是自己写的 可能是别人写的 有的人会乱写)
-//         // x可能还是一个promise 那么就让这个promise执行即可
-//         // {then:{}}
-//         // 这里的逻辑不单单是自己的 还有别人的 别人的promise 可能既会调用成功 也会调用失败
-//         if (typeof then === "function") {
-//           then.call(
-//             x,
-//             (y) => {
-//               // 返回promise后的成功结果
-//               // 递归直到解析成普通值为止
-//               if (called) return; // 防止多次调用
-//               called = true;
-//               // 递归 可能成功后的结果是一个promise 那就要循环的去解析
-//               resolvePromise(promise2, y, resolve, reject);
-//             },
-//             (err) => {
-//               // promise的失败结果
-//               if (called) return;
-//               called = true;
-//               reject(err);
-//             }
-//           );
-//         } else {
-//           resolve(x);
-//         }
-//       } catch (e) {
-//         if (called) return;
-//         called = true;
-//         reject(e);
-//       }
-//     } else {
-//       // 如果x是一个常量
-//       resolve(x);
-//     }
-//   }
-
-//   ///
-
-//   const promise2 = new myPromise((resolve, reject) => {
-//     if (this.status === status.resolved) {
-//       //获取回调的返回值
-//       try {
-//         // 当执行成功回调的时候 可能会出现异常，那就用这个异常作为promise2的错误的结果
-//         let x = onFulfilled(vm.value);
-//         //执行完当前成功回调后返回结果可能是promise
-//         resolvePromise(promise2, x, resolve, reject);
-//       } catch (e) {
-//         reject(e);
-//       }
-//     }
-
-//     if (this.status === status.rejected) {
-//       //获取回调的返回值
-//       try {
-//         let x = onRejected(vm.reason);
-//         resolvePromise(promise2, x, resolve, reject);
-//       } catch (e) {
-//         reject(e);
-//       }
-//     }
-
-//     // 如果异步执行则位pending状态
-//     if (this.status === status.pending) {
-//       // 保存回调函数
-//       this.resolvedCallbacks.push(() => {
-//         //获取回调的返回值
-//         try {
-//           let x = onFulfilled(vm.value);
-//           resolvePromise(promise2, x, resolve, reject);
-//         } catch (e) {
-//           reject(e);
-//         }
-//       });
-
-//       this.rejectedCallbacks.push(() => {
-//         //获取回调的返回值
-//         try {
-//           let x = onRejected(vm.reason);
-//           resolvePromise(promise2, x, resolve, reject);
-//         } catch (e) {
-//           reject(e);
-//         }
-//       });
-//     }
-//   });
-
-//   return promise2;
-// };
-
-////////
-
-// let promise = new myPromise((resolve, reject) => {
-//   setTimeout(() => {
-//     if (Math.random() > 0.5) {
-//       resolve("成功");
-//     } else {
-//       reject("失败");
-//     }
-//   });
-// });
-
-// promise.then(
-//   (data) => {
-//     console.log("success" + data);
-//   },
-//   (err) => {
-//     console.log("err" + err);
-//   }
-// );
+test
+  .then()
+  .then(
+    (result) => {
+      console.log(`success:${result}`);
+      console.log("then1");
+      return result;
+    },
+    (error) => {
+      console.log(`error:${error}`);
+      return `error:${error}`;
+    }
+  )
+  .then()
+  .then(function (result) {
+    if (result != undefined && result != "") console.log("get the data");
+    console.log(result);
+  });
